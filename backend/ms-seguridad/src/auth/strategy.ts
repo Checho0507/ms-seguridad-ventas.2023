af5d76957af53b5fa4a6fd6b76ecccee0f1175b8
Checho <checho.mlj@gmail.com>
2023-03-18 17:23:56 -0500

    Consulta de permisos de un rol en la estrategia LB4

--- backend/ms-seguridad/src/auth/strategy.ts
+++ backend/ms-seguridad/src/auth/strategy.ts
@@ -1,17 +1,80 @@
-import {AuthenticationStrategy} from '@loopback/authentication';
-import {Request} from '@loopback/rest';
+import {AuthenticationBindings, AuthenticationMetadata, AuthenticationStrategy} from '@loopback/authentication';
+import {inject, service} from '@loopback/core';
+import {repository} from '@loopback/repository';
+import {HttpErrors, Request} from '@loopback/rest';
 import {UserProfile} from '@loopback/security';
+import parseBearerToken from 'parse-bearer-token';
+import {RolMenuRepository} from '../repositories';
+import {SeguridadService} from '../services';
 
 export class AuthStrategy implements AuthenticationStrategy {
   name = 'auth';
 
   constructor(
-
+    @service(SeguridadService)
+    private servicioSeguridad: SeguridadService,
+    @inject(AuthenticationBindings.METADATA)
+    private metadata: AuthenticationMetadata,
+    @repository(RolMenuRepository)
+    private repositorioRolMenu: RolMenuRepository
   ) { }
 
+  /**
+   * Autenticación d eun usuario frente a una acción en la base de datos
+   * @param request la solicitud con el token
+   * @returns el perfil de usuario, undefined cuando no tiene permiso o un httpError
+   */
   async authenticate(request: Request): Promise<UserProfile | undefined> {
-    console.log("Ejecutando estrategia")
+    const token = parseBearerToken(request);
+    if (token) {
+      const idRol = this.servicioSeguridad.obtenerRolDesdeToken(token);
+      const idMenu: string = this.metadata.options![0];
+      const accion: string = this.metadata.options![1];
 
-    return undefined;
+      const permiso = await this.repositorioRolMenu.findOne({
+        where: {
+          rolId: idRol,
+          menuId: idMenu
+        }
+      });
+      let continuar = false;
+      if (permiso) {
+        switch (accion) {
+          case "guardar":
+            continuar = permiso.guardar;
+            break;
+          case "editar":
+            continuar = permiso.editar;
+            break;
+          case "listar":
+            continuar = permiso.listar;
+            break;
+          case "eliminar":
+            continuar = permiso.eliminar;
+            break;
+          case "descargar":
+            continuar = permiso.descargar;
+            break;
+
+          default:
+            throw new HttpErrors[401]("No es posible ejecutar la acción porque no existe")
+            break;
+        }
+        if (continuar) {
+          const perfil: UserProfile = Object.assign({
+            permitido: "OK"
+          });
+          return perfil;
+        } else {
+          return undefined;
+        }
+      } else {
+        throw new HttpErrors[401]("No es posible ejecutar la acción por falta de permisos");
+      }
+
+    } else {
+      throw new HttpErrors[401]("No es posible ejecutar la acción por falta de un token");
+    }
+
   }
 }
--- backend/ms-seguridad/src/config/seguridad.config.ts
+++ backend/ms-seguridad/src/config/seguridad.config.ts
@@ -1,3 +1,9 @@
 export namespace ConfiguracionSeguridad {
-  export const claveJWT = "Admin@2023*"
+  export const claveJWT = "Admin@2023*";
+  export const menuUsuarioId = "640f9c70bc8cb525a0d9f6f5";
+  export const listarAccion = "listar";
+  export const guardarAccion = "guardar";
+  export const editarAccion = "editar";
+  export const eliminarAccion = "eliminar";
+  export const descargarAccion = "descargar";
 }
